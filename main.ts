6dda02b4e67c85a5899e7bb830c34143b3a33019
eggo-waffle <54997527+eggo-waffle@users.noreply.github.com>
2019-10-14 11:30:27 -0700

    ahhhh

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 
 brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
-    brick.setStatusLight(StatusLight.GreenFlash)
+    brick.setStatusLight(StatusLight.RedFlash)
     brick.showString("Hello world", 1)
     brick.showString("Everything Is Awesome!", 2)
+    brick.showString("jk lol", 3)
 })
