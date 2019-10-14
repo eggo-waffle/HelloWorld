1ca6ce4a3f5b7af1d013886f9c9165958e32da64
eggo-waffle <54997527+eggo-waffle@users.noreply.github.com>
2019-10-14 11:28:10 -0700

    add button press+light

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
 
+brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
+    brick.setStatusLight(StatusLight.GreenFlash)
+    brick.showString("Hello world", 1)
+    brick.showString("Everything Is Awesome!", 2)
+})
